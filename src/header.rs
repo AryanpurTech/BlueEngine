1fc9c456249297f7d8609acb5bac9d809c1e60e7
Elham Aryanpur <elhamaryanpur5@gmail.com>
2021-09-23 22:23:53 +0300

    Added some documentation and official release of 0.1.9!!!!

--- src/header.rs
+++ src/header.rs
@@ -148,15 +148,48 @@ pub struct Object {
     pub color: uniform_type::Array,
 }
 
+/// The engine is the main starting point of using the Blue Engine. Everything that runs on Blue Engine will be under this struct.
+/// The structure of engine is monolithic, but the underlying data and the way it works is not.
+/// It gives a set of default data to work with, but also allow you to go beyond that and work as low level as you wish to.
+///
+/// You can also use the Engine to build you own custom structure the way you wish for it to be. Possibilities are endless!
+///
+/// To start using the Blue Engine, you can start by creating a new Engine like follows:
+/// ```
+/// use blue_engine::header::{Engine, WindowDescriptor};
+///
+/// fn main() {
+///     let engine = Engine::new(WindowDescriptor::default()).expect("Couldn't create the engine");
+/// }
+/// ```
+/// The WindowDescriptor simply holds what features you would like for your window. If you are reading this on later version of
+/// the engine, you might be able to even run the engine in headless mode meaning there would not be a need for a window and the
+/// renders would come as image files.
+///
+/// If you so wish to have a window, you would need to start a window update loop. The update loop of window runs a frame every few milisecond,
+/// and gives you details of what is happening during this time, like input events. You can also modify existing parts of the engine during
+/// this update loop, such as changing camera to look differently, or creating a new object on the scene, or even changing window details!
+///
+/// The update loop is just a method of the Engine struct that have one argument which is a callback function.
+/// ```
+///
+/// ```
+/// [THE DATA HERE IS WORK IN PROGRESS!]
 pub struct Engine {
+    /// The renderer does exactly what it is called. It works with the GPU to render frames according to the data you gave it.
     pub renderer: Renderer,
-    pub event_loop: winit::event_loop::EventLoop<()>,
+    // The event_loop handles the events of the window and inputs, so it's used internally
+    pub(crate) event_loop: winit::event_loop::EventLoop<()>,
+    /// The window handles everything about window and inputs. This includes ability to modify window and listen to input devices for changes.
     pub window: winit::window::Window,
+    /// The object system is a way to make it easier to work with the engine. Obviously you can work without it, but it's for those who
+    /// do not have the know-how, or wish to handle all the work of rendering data manually.
     pub objects: Vec<Object>,
+    /// The camera handles the way the scene looks when rendered. You can modify everything there is to camera through this.
     pub camera: Camera,
 }
 
-// Container for pipeline values. Each pipeline takes only 1 vertex shader, 1 fragment shader, 1 texture data, and optionally a vector of uniform data.
+/// Container for pipeline values. Each pipeline takes only 1 vertex shader, 1 fragment shader, 1 texture data, and optionally a vector of uniform data.
 #[derive(Debug, Clone, Copy)]
 pub struct Pipeline {
     pub shader_index: usize,
@@ -165,15 +198,20 @@ pub struct Pipeline {
     pub uniform_index: Option<usize>,
 }
 
-// Container for vertex and index buffer
+/// Container for vertex and index buffer
 pub struct VertexBuffers {
+    /// An array of vertices. A vertex is a point in 3D space containing an X, Y, and a Z coordinate between -1 and +1
     pub vertex_buffer: wgpu::Buffer,
+    /// An array of indices. Indices are a way to reuse vertices, this in turn helps greatly in reduction of amount of vertices needed to be sent to the GPU
     pub index_buffer: wgpu::Buffer,
     pub length: u32,
 }
 
+/// Shaders are programs that runs on the GPU
 pub type Shaders = wgpu::RenderPipeline;
+/// Uniform Buffers are small amount of data that are sent from CPU to GPU
 pub type UniformBuffers = wgpu::BindGroup;
+/// Textures are image data that are sent to GPU to be set to a surface
 pub type Textures = wgpu::BindGroup;
 
 // Main renderer class. this will contain all methods and data related to the renderer
@@ -182,6 +220,7 @@ pub struct Renderer {
     pub(crate) device: wgpu::Device,
     pub(crate) queue: wgpu::Queue,
     pub(crate) config: wgpu::SurfaceConfiguration,
+    /// This is the resolution that the renderer works with
     pub size: winit::dpi::PhysicalSize<u32>,
     pub(crate) texture_bind_group_layout: wgpu::BindGroupLayout,
     pub(crate) default_uniform_bind_group_layout: wgpu::BindGroupLayout,
@@ -253,6 +292,7 @@ pub enum UniformBuffer {
     Float(&'static str, uniform_type::Float),
 }
 
+/// This function helps in converting pixel value to the value that is between -1 and +1
 pub fn normalize(value: f32, max: u32) -> f32 {
     let mut result = value / max as f32;
 
--- src/utils/camera.rs
+++ src/utils/camera.rs
@@ -16,6 +16,7 @@ use anyhow::Result;
 use super::default_resources::DEFAULT_MATRIX_4;
 
 impl Camera {
+    /// Creates a new camera. this should've been automatically done at the time of creating an engine
     pub fn new(renderer: &Renderer) -> Result<Self> {
         let mut camera = Self {
             eye: glm::vec3(0.0, 0.0, 0.0),
@@ -33,6 +34,7 @@ impl Camera {
         Ok(camera)
     }
 
+    /// Updates the view uniform matrix that decides how camera works
     pub fn build_view_projection_matrix(&mut self) -> Result<()> {
         let view = glm::ext::look_at_rh(self.eye, self.target, self.up);
         let proj = glm::ext::perspective::<f32>(self.fov, self.aspect, self.near, self.far);
@@ -42,10 +44,12 @@ impl Camera {
         Ok(())
     }
 
+    /// Returns a matrix uniform buffer from camera data that can be sent to GPU
     pub fn camera_uniform_buffer(&self) -> Result<Matrix> {
         Ok(Matrix::from_glm(self.view_data))
     }
 
+    /// Sets the eye of camera
     pub fn set_eye(&mut self, new_eye: [f32; 3]) -> Result<()> {
         self.eye = glm::vec3(new_eye[0], new_eye[1], new_eye[2]);
         self.build_view_projection_matrix()?;
@@ -53,6 +57,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets the target of camera
     pub fn set_target(&mut self, new_target: [f32; 3]) -> Result<()> {
         self.target = glm::vec3(new_target[0], new_target[1], new_target[2]);
         self.build_view_projection_matrix()?;
@@ -60,6 +65,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets the up of camera
     pub fn set_up(&mut self, new_up: [f32; 3]) -> Result<()> {
         self.up = glm::vec3(new_up[0], new_up[1], new_up[2]);
         self.build_view_projection_matrix()?;
@@ -67,6 +73,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets the field of view of camera
     pub fn set_fov(&mut self, new_fov: f32) -> Result<()> {
         self.fov = new_fov;
         self.build_view_projection_matrix()?;
@@ -74,6 +81,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets how far camera can look
     pub fn set_far(&mut self, new_far: f32) -> Result<()> {
         self.far = new_far;
         self.build_view_projection_matrix()?;
@@ -81,6 +89,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets how near the camera can look
     pub fn set_near(&mut self, new_near: f32) -> Result<()> {
         self.near = new_near;
         self.build_view_projection_matrix()?;
@@ -88,6 +97,7 @@ impl Camera {
         Ok(())
     }
 
+    /// Sets the aspect ratio of the camera
     pub fn set_aspect(&mut self, new_aspect: f32) -> Result<()> {
         self.aspect = new_aspect;
         self.build_view_projection_matrix()?;
@@ -95,6 +105,7 @@ impl Camera {
         Ok(())
     }
 
+    /// This builds a uniform buffer data from camera view data that is sent to the GPU in next frame
     pub fn update_view_projection(&mut self, renderer: &mut Renderer) -> Result<()> {
         if self.changed {
             let updated_buffer = renderer
