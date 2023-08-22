334dbc00a303e82b585a031d1e998563b1fc4455
Elham Aryanpur <elhamaryanpur5@gmail.com>
2023-08-22 10:40:24 +0300

    chore: moved files to correct places

--- BlueEngine-master/src/primitive_shapes.rs
+++
@@ -1,4 +0,0 @@
-pub mod three_dimensions;
-pub mod two_dimensions;
-pub use three_dimensions::*;
-pub use two_dimensions::*;
--- BlueEngine-master/src/utils.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Blue Engine by Elham Aryanpur
- *
- * The license is same as the one on the root.
-*/
-
-pub mod camera;
-pub mod default_resources;
