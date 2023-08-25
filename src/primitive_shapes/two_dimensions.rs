d24a865e4d61f5ce321a686cfd5f0784b7d3c9a2
Gipson62 <J.H.Gipson62@gmail.com>
2023-08-25 20:38:43 +0200

    Add rectangle as a primitive shape

--- src/primitive_shapes/two_dimensions.rs
+++ src/primitive_shapes/two_dimensions.rs
@@ -78,3 +78,37 @@ pub fn square(
 
     Ok(())
 }
+
+/// Create a 2D rectangle based on a width and height
+pub fn rectangle(width: f32, height: f32, name: impl StringBuffer, settings: ObjectSettings, renderer: &mut Renderer, objects: &mut ObjectStorage) -> anyhow::Result<()> {
+    objects.new_object(
+        name.clone(),
+        vec![
+            Vertex {
+                position: [width/2.0, height/2.0, 0.0],
+                uv: [1.0, 0.0],
+                normal: [0f32, 0f32, 0f32],
+            },
+            Vertex {
+                position: [width/2.0, -height/2.0, 0.0],
+                uv: [1.0, 1.0],
+                normal: [0f32, 0f32, 0f32],
+            },
+            Vertex {
+                position: [-width/2.0, -height/2.0, 0.0],
+                uv: [0.0, 1.0],
+                normal: [0f32, 0f32, 0f32],
+            },
+            Vertex {
+                position: [-width/2.0, height/2.0, 0.0],
+                uv: [0.0, 0.0],
+                normal: [0f32, 0f32, 0f32],
+            },
+        ],
+        vec![2, 1, 0, 2, 0, 3],
+        settings,
+        renderer
+    )?;
+
+    Ok(())
+}
