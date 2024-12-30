2f4fdeac92688e9226bc6e4e4fbf900067c87fe8
Elham Aryanpur <elhamaryanpur5@gmail.com>
2024-12-30 21:32:05 +0300

    feat: added dynamic linking directly to the engine

---
+++ crates/blue_engine_dynamic/src/lib.rs
@@ -0,0 +1,1 @@
+pub use blue_engine_core::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
-#[cfg(not(feature = "dynamic_linking"))]
+#[cfg(not(feature = "dynamic_link"))]
 pub use blue_engine_core::*;
+
+#[cfg(all(feature = "dynamic_link", not(target_family = "wasm")))]
+pub use blue_engine_dynamic::*;
